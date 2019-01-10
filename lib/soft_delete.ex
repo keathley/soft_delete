321dff0f77dfe5de6a45817873b9a928b6f1c850
Chris Keathley <c@keathley.io>
2019-01-10 16:17:36 -0500

    initial commit

---
+++ lib/soft_delete.ex
@@ -0,0 +1,18 @@
+defmodule SoftDelete do
+  @moduledoc """
+  Documentation for SoftDelete.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SoftDelete.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/soft_delete_test.exs
@@ -0,0 +1,8 @@
+defmodule SoftDeleteTest do
+  use ExUnit.Case
+  doctest SoftDelete
+
+  test "greets the world" do
+    assert SoftDelete.hello() == :world
+  end
+end
